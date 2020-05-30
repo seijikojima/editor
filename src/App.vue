45a584db7fb53cc4683da601a1f5d45202c58b7d
sabosan <sabosan331@gmail.com>
2020-05-31 01:47:55 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,209 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="quill-editor">
+    <slot name="toolbar"></slot>
+    <div ref="editor"></div>
+    <button class="btn" v-on:click="sendArticle()">SendArticle</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// require sources
+import _Quill from 'quill'
+const Quill = window.Quill || _Quill
 
+// import { ImageResize } from 'quill-image-resize-module';
+// Quill.register('modules/imageResize', ImageResize);
+const defaultOptions = {
+  theme: 'snow',
+  boundary: document.body,
+  modules: {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'direction': 'rtl' }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean'],
+      ['link', 'image', 'video']
+    ],
+  },
+  placeholder: 'Insert text here ...',
+  readOnly: false
+}
+// pollfill
+if (typeof Object.assign != 'function') {
+  Object.defineProperty(Object, 'assign', {
+    value(target, varArgs) {
+      if (target == null) {
+        throw new TypeError('Cannot convert undefined or null to object')
+      }
+      const to = Object(target)
+      for (let index = 1; index < arguments.length; index++) {
+        const nextSource = arguments[index]
+        if (nextSource != null) {
+          for (const nextKey in nextSource) {
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey]
+            }
+          }
+        }
+      }
+      return to
+    },
+    writable: true,
+    configurable: true
+  })
+}
+// export
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'quill-editor',
+  data() {
+    return {
+      _options: {},
+      _content: '',
+      defaultOptions
+    }
+  },
+  props: {
+    content: String,
+    value: String,
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    options: {
+      type: Object,
+      required: false,
+      default: () => ({})
+    },
+    globalOptions: {
+      type: Object,
+      required: false,
+      default: () => ({})
+    }
+  },
+  mounted() {
+    this.initialize()
+  },
+  beforeDestroy() {
+    this.quill = null
+    delete this.quill
+  },
+  methods: {
+
+    sendArticle(){
+      console.log(this._content)
+    },
+
+    // Init Quill instance
+    initialize() {
+      if (this.$el) {
+        // Options
+        this._options = Object.assign({}, this.defaultOptions, this.globalOptions, this.options)
+        // Instance
+        this.quill = new Quill(this.$refs.editor, this._options)
+        
+        this.quill.enable(false)
+        // Set editor content
+        if (this.value || this.content) {
+          this.quill.pasteHTML(this.value || this.content)
+        }
+        // Disabled editor
+        if (!this.disabled) {
+          this.quill.enable(true)
+        }
+        // Mark model as touched if editor lost focus
+        this.quill.on('selection-change', range => {
+          if (!range) {
+            this.$emit('blur', this.quill)
+          } else {
+            this.$emit('focus', this.quill)
+          }
+        })
+        // Update model if text changes
+        this.quill.on('text-change', (delta, oldDelta, source) => {
+          let html = this.$refs.editor.children[0].innerHTML
+          const quill = this.quill
+          const text = this.quill.getText()
+          if (html === '<p><br></p>') html = ''
+          this._content = html
+          this.$emit('input', this._content)
+          this.$emit('change', { html, text, quill })
+        })
+        // Emit ready event
+        this.$emit('ready', this.quill)
+      }
+    }
+  },
+  watch: {
+    // Watch content change
+    content(newVal, oldVal) {
+      if (this.quill) {
+        if (newVal && newVal !== this._content) {
+          this._content = newVal
+          this.quill.pasteHTML(newVal)
+        } else if(!newVal) {
+          this.quill.setText('')
+        }
+      }
+    },
+    // Watch content change
+    value(newVal, oldVal) {
+      if (this.quill) {
+        if (newVal && newVal !== this._content) {
+          this._content = newVal
+          this.quill.pasteHTML(newVal)
+        } else if(!newVal) {
+          this.quill.setText('')
+        }
+      }
+    },
+    // Watch disabled change
+    disabled(newVal, oldVal) {
+      if (this.quill) {
+        this.quill.enable(!newVal)
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+    box-sizing: border-box;
 }
+body {
+    margin : 0;
+}
+
+.quill-editor{
+  border : solid 1px;
+}
+
+.btn {
+	display: block;
+	position: relative;
+	width: 100px;
+	padding: 0.8em;
+	text-align: center;
+	text-decoration: none;
+	color: #000;
+	background: #fff;
+}
+.btn:hover {
+	 cursor: pointer;
+	 text-decoration: none;
+   color:#fff;
+	background:#000;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+Vue.use(VueQuillEditor, /* { default global options } */)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
